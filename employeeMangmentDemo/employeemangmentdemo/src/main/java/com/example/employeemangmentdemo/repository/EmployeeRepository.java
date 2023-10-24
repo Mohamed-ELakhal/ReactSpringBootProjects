d2df3ead5750a18d17940d5c5078029839abd268
Mohamed-ELakhal <92973153+Mohamed-ELakhal@users.noreply.github.com>
2023-10-24 06:39:53 +0300

    Delete employeeMangmentDemo directory

--- employeeMangmentDemo/employeemangmentdemo/src/main/java/com/example/employeemangmentdemo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeemangmentdemo.repository;
-
-import com.example.employeemangmentdemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
