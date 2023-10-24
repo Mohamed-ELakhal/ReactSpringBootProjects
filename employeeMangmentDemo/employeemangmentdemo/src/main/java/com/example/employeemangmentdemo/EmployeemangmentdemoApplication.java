26e95d2a7f5d8ca006640a8e30dc0520f8937c2a
Mohamed-ELakhal <mohamedelakhal92@gmail.com>
2023-10-24 06:28:04 +0300

    add Employee Management Project

---
+++ employeeMangmentDemo/employeemangmentdemo/src/main/java/com/example/employeemangmentdemo/EmployeemangmentdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeemangmentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemangmentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemangmentdemoApplication.class, args);
+	}
+
+}
