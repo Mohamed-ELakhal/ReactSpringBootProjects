e2dfd4b3dbfce1098e72d780be6ff49c18610618
Mohamed-ELakhal <92973153+Mohamed-ELakhal@users.noreply.github.com>
2023-10-24 06:27:41 +0300

    Delete Registeration Form/demo directory

--- Registeration Form/demo/src/main/java/com/example/demo/Dao/EmployeeRepositry.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Dao;
-
-import com.example.demo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepositry extends JpaRepository<Employee,Integer> {
-}
